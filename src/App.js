d2c9c229716e8d4ded43e53f9df861b909e4925f
R-Aranda <rupertaranda@gmail.com>
2022-07-13 09:05:26 -0400

    updated if data.copyright does not exist

--- src/App.js
+++ src/App.js
@@ -5,19 +5,31 @@ const App = (props) => {
   const fetchNasaData = async () => {
     const response = await fetch("/api/nasa");
     const nasaData = await response.json();
-
     setNasa(nasaData);
   };
 
   useEffect(() => {
     fetchNasaData();
   }, []);
+
+  let copyright;
+
+  if (!nasa.copyright) {
+    copyright = null;
+  } else {
+    copyright = (
+      <div>
+        <strong>Copyright & Credit:</strong> {nasa.copyright}
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Nasa's Astronomy Picture of the Day</h1>
       <img src={nasa.hdurl} />
       <h3>{nasa.title}</h3>
-      <strong>Copyright & Credit:</strong> {nasa.copyright}
+      {copyright}
       <p>"{nasa.explanation}"</p>
       <p>{nasa.date}</p>
     </div>
